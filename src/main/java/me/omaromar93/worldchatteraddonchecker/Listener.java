5b06629411dcf545b6d520bf63eeb2f5b665cd06
OmarOmar93 <omartemhosma7@gmail.com>
2024-06-22 03:15:41 +0300

    changes

--- src/main/java/me/omaromar93/worldchatteraddonchecker/Listener.java
+++ src/main/java/me/omaromar93/worldchatteraddonchecker/Listener.java
@@ -16,10 +16,15 @@ public class Listener implements WorldChatterAPI {
         System.out.println("ChatLock Toggled to " + b);
     }
 
+    @Override
+    public void updateChecked(boolean b, boolean b1) {
+        System.out.println("Is it updated?: " + (b ? "yes" : "no"));
+        System.out.println("Is development build?: " + (b1 ? "yes" : "no"));
+    }
 
     @Override
     public void updateChecked(boolean b) {
-        System.out.println("Is it updated?: " + (b ? "yes" : "no"));
+        // depreciated
     }
 
     @Override
